713cfcb272cab783d3576880121f4c3a2c765660
Michael McCall <mmccal43@uncc.edu>
2021-11-10 12:34:29 -0500

    flask app #3 - created initial app file

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -12,10 +12,27 @@ app = Flask(__name__)     # create an app
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
-    a_user = {'name': 'Mogli', 'email':'mogli@uncc.edu'}
+    a_user = {'name': 'Michael McCall', 'email':'mmccal43@uncc.edu'}
     
     return render_template('index.html', user = a_user)
 
+@app.route('/notes')
+def getNotes():
+    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+             }
+    
+    return render_template('notes.html', notes=notes)
+
+@app.route('/notes/<note_id>')
+def get_note(note_id):
+    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+             }
+    
+    return render_template('note.html', note=notes[int(note_id)])
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
