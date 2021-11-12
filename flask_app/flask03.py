51cfd20b9569754a683c05517207c96f308bbb7c
Michael McCall <mmccal43@uncc.edu>
2021-11-12 03:13:31 -0500

    flask app #3 - added new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -17,7 +17,7 @@ def index():
     return render_template('index.html', user = a_user)
 
 @app.route('/notes')
-def getNotes():
+def get_notes():
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
@@ -33,6 +33,11 @@ def get_note(note_id):
              }
     
     return render_template('note.html', note=notes[int(note_id)])
+
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
+    return render_template('new.html', user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
